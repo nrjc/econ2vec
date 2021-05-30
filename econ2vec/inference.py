85715dc4833818f567a1b715797f34711c1262a0
Funaizhang <naifuzhang@gmail.com>
2021-05-31 00:29:58 +0800

    Implement cos similartiy

--- econ2vec/inference.py
+++ econ2vec/inference.py
@@ -13,5 +13,15 @@ class Econ2VecInferencer:
     embedding_filename: str = "out.vec"
 
     def __post_init__(self):
-        self.skip_gram_model = SkipGramContinuousModel(emb_size=1)
-        self.skip_gram_model.load_embedding(file_name=self.embedding_filename)
+        self.model = SkipGramContinuousModel(emb_size=1)
+        self.model.load_embedding(file_name=self.embedding_filename)
+
+    def ticker2emb(self, ticker):
+        id = self.model.ts2id[ticker]
+        emb = self.model.u_embeddings.weight.t()[id, :]
+        return emb
+
+    def cos_sim(self, ticker1, ticker2):
+        emb1, emb2 = self.ticker2emb(ticker1), self.ticker2emb(ticker2)
+        cos = nn.CosineSimilarity(dim=0, eps=1e-8)
+        return cos(emb1, emb2)
